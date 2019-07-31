64b41047672ff3065dbbd1b23045e33c021c9102
janejungsdf <33800511+janejungsdf@users.noreply.github.com>
2019-08-01 00:17:42 +0900

    feat: restructuring

--- blog/blog/blogproject/blogapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Blog
-
-def home(request):
-    blogs = Blog.objects
-    return render(request, 'home.html', {'blogs':blogs})
-
-
-# Create your views here.
